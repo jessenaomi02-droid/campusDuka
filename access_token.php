3a67a2a4b915c9805ee4c207cacaff949e0dcf69
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-03-10 20:24:10 +0300

    Update access_token.php

--- access_token.php
+++ access_token.php
@@ -2,34 +2,30 @@
 
 include('config.php');
 
-$credentials = base64_encode($consumerKey . ":" . $consumerSecret);
+$consumerKey = $consumerKey;
+$consumerSecret = $consumerSecret;
+
+$credentials = base64_encode($consumerKey.":".$consumerSecret);
 
 $url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
-$curl = curl_init();
+$curl = curl_init($url);
 
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
     "Authorization: Basic ".$credentials
-));
+]);
+
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($curl);
 
-if ($response === false) {
-    echo "Curl Error: ".curl_error($curl);
+if(curl_errno($curl)){
+    echo "Curl error: ".curl_error($curl);
     exit;
 }
 
-$result = json_decode($response, true);
+curl_close($curl);
 
-if (!isset($result['access_token'])) {
-    echo "Token Error Response:";
-    print_r($result);
-    exit;
-}
-
-echo $result['access_token'];
+echo $response;
 
 ?>
-
