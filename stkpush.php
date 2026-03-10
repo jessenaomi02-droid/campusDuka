99db61f5992cedff3dd6f054560d3b9c0f1c25a5
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-03-10 21:17:04 +0300

    Update stkpush.php

--- stkpush.php
+++ stkpush.php
@@ -1,34 +1,22 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 
 include('config.php');
 
 /* GET ACCESS TOKEN */
 
-$credentials = base64_encode($consumerKey.":".$consumerSecret);
+$response = file_get_contents("https://campusduka.onrender.com/access_token.php");
+$result = json_decode($response);
 
-$token_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-
-$curl = curl_init($token_url);
-
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-"Authorization: Basic ".$credentials
-));
-
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$token_response = curl_exec($curl);
-
-$token = json_decode($token_response)->access_token;
-
-curl_close($curl);
+$access_token = $result->access_token;
 
 
 /* GENERATE PASSWORD */
 
 $timestamp = date("YmdHis");
+
 $password = base64_encode($shortcode.$passkey.$timestamp);
 
 
@@ -37,17 +25,17 @@ $password = base64_encode($shortcode.$passkey.$timestamp);
 $url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
 
 $data = array(
-"BusinessShortCode"=>$shortcode,
-"Password"=>$password,
-"Timestamp"=>$timestamp,
-"TransactionType"=>"CustomerPayBillOnline",
-"Amount"=>"1",
-"PartyA"=>"254708374149",
-"PartyB"=>$shortcode,
-"PhoneNumber"=>"254708374149",
-"CallBackURL"=>$callbackUrl,
-"AccountReference"=>"CampusDuka",
-"TransactionDesc"=>"Test Payment"
+    "BusinessShortCode"=>$shortcode,
+    "Password"=>$password,
+    "Timestamp"=>$timestamp,
+    "TransactionType"=>"CustomerPayBillOnline",
+    "Amount"=>"1",
+    "PartyA"=>"254708374149",
+    "PartyB"=>$shortcode,
+    "PhoneNumber"=>"254708374149",
+    "CallBackURL"=>$callbackUrl,
+    "AccountReference"=>"CampusDuka",
+    "TransactionDesc"=>"Test Payment"
 );
 
 $payload = json_encode($data);
@@ -55,8 +43,8 @@ $payload = json_encode($data);
 $curl = curl_init($url);
 
 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-"Content-Type: application/json",
-"Authorization: Bearer ".$token
+    "Content-Type: application/json",
+    "Authorization: Bearer ".$access_token
 ));
 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -70,5 +58,4 @@ echo $response;
 curl_close($curl);
 
 ?>
-    
 
