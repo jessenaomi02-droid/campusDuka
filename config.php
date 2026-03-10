6718876a64e9626745f05714966c1c1173615818
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-03-10 17:58:20 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$consumerKey = "YOUR_CONSUMER_KEY";
+$consumerSecret = "YOUR_CONSUMER_SECRET";
+
+$shortcode = "174379"; // sandbox shortcode
+$passkey = "YOUR_PASSKEY";
+
+$callbackUrl = "https://your-render-url.onrender.com/callback.php";
+
+?>
