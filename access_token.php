c4acfd0bc2a9698e99d855504ae97b36813f790f
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-03-10 20:18:15 +0300

    Update access_token.php

--- access_token.php
+++ access_token.php
@@ -10,14 +10,26 @@ $curl = curl_init();
 
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    "Authorization: Basic $credentials"
+    "Authorization: Basic ".$credentials
 ));
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($curl);
 
-$result = json_decode($response);
+if ($response === false) {
+    echo "Curl Error: ".curl_error($curl);
+    exit;
+}
 
-echo $result->access_token;
+$result = json_decode($response, true);
+
+if (!isset($result['access_token'])) {
+    echo "Token Error Response:";
+    print_r($result);
+    exit;
+}
+
+echo $result['access_token'];
 
 ?>
+
