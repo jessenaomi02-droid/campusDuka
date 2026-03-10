2e312a14dd468dca3d6cce58cbd80bb4beab5395
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-03-10 20:40:43 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-$consumerKey = "YOUR_CONSUMER_KEY";
-$consumerSecret = "YOUR_CONSUMER_SECRET";
+$consumerKey = "ewtAv4VAGXPmyYYixt53reT8fea8jF8h2Nx2FIQWvD6HWaXB";
+$consumerSecret = "PuRoGTNg4NeRJYsswH32GGVIldt89Kosyejwp2TGNb95AMXw9iLOKG0njghcARS9";
 
 $shortcode = "174379";
 
 $passkey = "bfb279f9aa9bdbcf158e97dd8a83e6e63c8f6b6c7e6f2b9baf4b5a2f1a9f7b";
 
-$callbackUrl = "https://your-render-url.onrender.com/callback.php";
+$callbackUrl = "https://campusduka.onrender.com/callback.php";
 
 ?>
