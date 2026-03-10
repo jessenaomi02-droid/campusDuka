512927dc55cb8a0e11b0fa19ffe9104abd539c82
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-03-10 20:28:53 +0300

    Update access_token.php

--- access_token.php
+++ access_token.php
@@ -1,31 +1,36 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include('config.php');
 
-$consumerKey = $consumerKey;
-$consumerSecret = $consumerSecret;
-
-$credentials = base64_encode($consumerKey.":".$consumerSecret);
+$credentials = base64_encode($consumerKey . ":" . $consumerSecret);
 
 $url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
-$curl = curl_init($url);
+$curl = curl_init();
 
-curl_setopt($curl, CURLOPT_HTTPHEADER, [
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array(
     "Authorization: Basic ".$credentials
-]);
-
+));
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $response = curl_exec($curl);
 
-if(curl_errno($curl)){
-    echo "Curl error: ".curl_error($curl);
+if ($response === false) {
+    echo "Curl Error: " . curl_error($curl);
     exit;
 }
 
-curl_close($curl);
+$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
+echo "HTTP Status Code: " . $httpCode . "<br><br>";
+echo "Response:<br>";
 echo $response;
+
+curl_close($curl);
 
 ?>
