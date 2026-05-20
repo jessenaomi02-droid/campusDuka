2a288b03b66f357ab1e39a835fc991edb1e9e0ff
jessenaomi02-droid <jessenaomi02@gmail.com>
2026-05-20 10:59:36 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,12 @@
-module.exports={}
+const {Pool}=require("pg");
+
+const pool=new Pool({
+connectionString:
+process.env.DATABASE_URL,
+
+ssl:{
+rejectUnauthorized:false
+}
+});
+
+module.exports=pool;
